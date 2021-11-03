9667df7525507918479319085a3f3751f12c9947
Erasmo Carlos <erasmocarlosrm@gmail.com>
2021-11-02 21:13:36 -0300

    Basic Static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/erasmocarlosrm" target="_blank" rel="noreferrer">Erasmo Carlos</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: </small></p>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
